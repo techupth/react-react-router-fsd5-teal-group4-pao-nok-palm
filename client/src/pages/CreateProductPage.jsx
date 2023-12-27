3be747c5ed9873a7680cd46277258e192c80e6e4
Pao <chaiwatt77@gmail.com>
2023-12-27 16:35:10 +0700

    fix:back button

--- client/src/pages/CreateProductPage.jsx
+++ client/src/pages/CreateProductPage.jsx
@@ -1,11 +1,14 @@
 import CreateProductForm from "../components/CreateProductForm";
+import { useNavigate } from "react-router-dom";
 
 function CreateProductPage() {
+
+  const navi = useNavigate()
   return (
     <div>
       <h1>Create Product Page</h1>
       <CreateProductForm />
-      <button>Back to Home</button>
+      <button onClick={()=>{navi("/")}}>Back to Home</button>
     </div>
   );
 }
